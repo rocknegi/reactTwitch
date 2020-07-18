195b2e5feee695ff2add532a947b03f9d609e944
unknown <rocknegi53@gmail.com>
2020-07-18 13:37:42 +0530

    add delete method use a generic componenet for modal

--- src/componenets/streams/StreamDelete.js
+++ src/componenets/streams/StreamDelete.js
@@ -1,13 +1,51 @@
 import React from 'react'
-import Modal from '../Modal'
+import { connect } from 'react-redux'
+import { fetchStream, deleteStream } from '../../actions'
 
-const StreamDelete = () => {
-    return (
-        <div>
-            StreamDelete
-            <Modal />
-        </div>
-    )
+import Modal from '../Modal'
+import history from '../../history';
+import { Link } from 'react-router-dom';
+
+class StreamDelete extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    renderAction() {
+        const { id } = this.props.match.params
+        console.log(id)
+        return (
+            <>
+                <button onClick={() => this.props.deleteStream(id)} className="ui button negative">Delete</button>
+                <Link to='/' className="ui button">Cancel</Link>
+            </>
+        )
+    }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return 'Are your sure you want to delete this stream?'
+        }
+        return `Are your sure you want to delete this stream with title : ${this.props.stream.title}`
+    }
+
+    render() {
+        return (
+            <Modal
+                actions={this.renderAction()}
+                title="Delete Stream"
+                content={this.renderContent()}
+                onDismiss={() => history.push('/')}
+            />
+        )
+    }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
--- src/componenets/streams/StreamList.js
+++ src/componenets/streams/StreamList.js
@@ -16,9 +16,11 @@ class StreamList extends React.Component {
                     <Link className="ui button primary"
                         to={`/streams/edit/${stream.id}`}
                     >Edit</Link>
-                    <button className="ui button negative">
+                    <Link
+                        to={`/streams/delete/${stream.id}`}
+                        className="ui button negative">
                         Delete
-                    </button>
+                    </Link>
                 </div>
             )
     }
